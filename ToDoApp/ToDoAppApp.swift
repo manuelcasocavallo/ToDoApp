17884074fec4870b8ce6e049db07bfe46c0bb3b0
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-05-10 16:01:13 +0200

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Manuel Casocavallo on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
